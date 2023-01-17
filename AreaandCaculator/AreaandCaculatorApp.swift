e9abd46f3332d6ec33bc7885f55df5aeff34c1a7
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-01-17 11:34:59 -0500

    Initial Commit

---
+++ AreaandCaculator/AreaandCaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaandCaculatorApp.swift
+//  AreaandCaculator
+//
+//  Created by Kiho Okazawa on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct AreaandCaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
