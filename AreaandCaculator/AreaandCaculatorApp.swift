19132db582e1c3ca9adff1344bf82ae96bb02aaf
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-01-17 11:38:56 -0500

    Set up an empty view to calculate a area of a rectangle

--- AreaandCaculator/AreaandCaculatorApp.swift
+++ AreaandCaculator/AreaandCaculatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AreaandCaculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RectangleView()
         }
     }
 }
---
+++ AreaandCaculator/View/RectangleView.swift
@@ -0,0 +1,20 @@
+//
+//  RectangleView.swift
+//  AreaandCaculator
+//
+//  Created by Kiho Okazawa on 2023-01-17.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleView()
+    }
+}
